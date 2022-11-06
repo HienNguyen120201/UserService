e58cb07b87a228a080828f30e8a03484df1b3422
nkpt <trangnkp@inspirelab.io>
2022-11-06 15:45:53 +0700

    Edit proto, add RegisterRequest implement

---
+++ go.mod
@@ -0,0 +1,32 @@
+module userservice
+
+go 1.19
+
+require (
+	entgo.io/ent v0.11.4
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	go.uber.org/zap v1.23.0
+	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167
+	google.golang.org/grpc v1.50.1
+	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	ariga.io/atlas v0.7.3-0.20221011160332-3ca609863edd // indirect
+	github.com/agext/levenshtein v1.2.1 // indirect
+	github.com/apparentlymart/go-textseg/v13 v13.0.0 // indirect
+	github.com/go-openapi/inflect v0.19.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/hcl/v2 v2.13.0 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/zclconf/go-cty v1.8.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+)
--- server/internal/server.go
+++ server/internal/server.go
@@ -2,15 +2,92 @@ package internal
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
 	pb "userservice/server/api"
+	"userservice/server/pkg/ent"
+	"userservice/server/pkg/ent/user"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc"
+)
+
+var (
+	port     = flag.Int("port", 50051, "The server port")
+	database *ent.Client
 )
 
 type server struct {
 	pb.UnimplementedUserServiceServer
+	db     *ent.Client
+	logger *zap.Logger
+}
+
+func NewLogger(lv zapcore.Level, pretty bool) (*zap.Logger, error) {
+	c := zap.NewDevelopmentConfig()
+	var opts []zap.Option
+	if pretty {
+		opts = append(opts, zap.AddStacktrace(zap.ErrorLevel))
+	}
+	level := zap.NewAtomicLevel()
+
+	if err := level.UnmarshalText([]byte(lv.String())); err != nil {
+		return nil, fmt.Errorf("Could not parse log level %s", lv.String())
+	}
+	c.Level = level
+	return c.Build(opts...)
+}
+
+func HashPwd(pwd string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(pwd), 10)
+	return string(bytes), err
+}
+
+func CreateToken(username string) (string, error) {
+	claims := jwt.MapClaims{
+		"authorized": true,
+		"username":   username,
+		"exp":        time.Now().Add(time.Hour * 12).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(os.Getenv("SECRET_JWT")))
 }
 
 func (s *server) Register(ctx context.Context, in *pb.RegisterRequest) (*pb.RegisterReply, error) {
-	return nil, nil
+	already, err := s.db.User.Query().Where(user.Username(in.Username)).Exist(ctx)
+	if err != nil {
+		s.logger.Sugar().Debug("Fail to get username:", err)
+	}
+	if already {
+		return nil, err //?
+	} else {
+		pwd, err := HashPwd(in.Password)
+		if err != nil {
+			s.logger.Sugar().Debug("Fail to hash password:", err)
+		}
+		res, err := s.db.User.Create().SetUsername(in.Username).SetPassword(pwd).SetName(in.Name).Save(ctx)
+		if err != nil {
+			s.logger.Sugar().Debug("Fail to register:", err)
+			return nil, err
+		}
+		token, err := CreateToken(res.Username)
+		if err != nil {
+			s.logger.Sugar().Debug("Fail to create token:", err)
+			return nil, err
+		}
+		return &pb.RegisterReply{
+			UserId: strconv.Itoa(res.ID),
+			Token:  token,
+		}, nil
+	}
 }
 func (s *server) Login(ctx context.Context, in *pb.LoginRequest) (*pb.LoginReply, error) {
 	return nil, nil
@@ -19,6 +96,38 @@ func (s *server) UpdateProfile(ctx context.Context, in *pb.UpdateProfileRequest)
 	return nil, nil
 }
 
-func RunGRPC(){
+func connectDB() *ent.Client {
+	db, err := ent.Open("mysql", "root:28010608@tcp(mysqldb:3306)/user", ent.Debug())
+	if err != nil {
+		log.Fatal("Fail to connect database: ", err)
+	}
+	if err = db.Schema.Create(context.Background()); err != nil {
+		log.Fatal("Fail to create schema resources: ", err)
+	}
+	database = db
+	return db
+}
+func RunGRPC() {
+	logger, err := NewLogger(zap.DebugLevel, true)
+	if err != nil {
+		logger.Sugar().Debug("Fail to initialize log:", err)
+	}
+	defer logger.Core().Sync()
 
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
+	if err != nil {
+		log.Fatal("Fail to listen:", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterUserServiceServer(s, &server{
+		db:     connectDB(),
+		logger: logger,
+	})
+	log.Println("grpc server listening at ", lis.Addr())
+	go func() {
+		log.Fatal("Fail to serve:", s.Serve(lis))
+	}()
+	defer database.Close()
 }
